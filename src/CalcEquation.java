8653be561972d77f2b4896562a6357e5ed84f61c
Stas Prystash <stas.prystash@gmail.com>
2019-06-24 18:16:30 +0300

    github

--- src/CalcEquation.java
+++ src/CalcEquation.java
@@ -6,14 +6,17 @@ public class CalcEquation {
 
     public static void main(String[] args) {
 
-        double a, b, c, d;
-
+        double a, b, c; //declaring coefficients of quadratic equation
+        double d; //declaring discriminant
         System.out.printf("Solving a quadratic equation: a*x^2+b*x+c=0" + "\n");
 
         a = validateAndGet("a");
         b = validateAndGet("b");
         c = validateAndGet("c");
 
+        /*
+       calculating the roots
+        */
         if (a == 0) {
 
             double x = solutionSimple(b, c);
@@ -26,11 +29,11 @@ public class CalcEquation {
 
             System.out.println("Solution:");
             if (d > 0) {
-                double[] result = solutionQudratic(a, b, d);
+                double[] result = solutionQuadratic(a, b, d);
                 System.out.println("x1 = " + result[0]);
                 System.out.println("x2 = " + result[1]);
             } else if (d == 0) {
-                double x = solutionQudratic(a, b);
+                double x = solutionQuadratic(a, b);
                 System.out.println("x = " + x);
 
             } else {
@@ -39,6 +42,10 @@ public class CalcEquation {
         }
     }
 
+    /*
+        Method that validates and gets a,b,c coefficients.
+        If a,b,c not numbers than method return error.
+    */
     private static double validateAndGet(String coeffName) {
         System.out.println("Enter coefficient '" + coeffName + "': ");
         while (!scanner.hasNextDouble()) {
@@ -49,11 +56,17 @@ public class CalcEquation {
         return scanner.nextDouble();
     }
 
+    /*
+       Method that calculates discriminant
+    */
     private static double discriminant(double a, double b, double c) {
         return Math.pow(b, 2) - (4 * a * c);
     }
 
-    private static double[] solutionQudratic(double a, double b, double d) {
+    /*
+        Method that calculates the roots of quadratic equation if discriminant > 0
+    */
+    private static double[] solutionQuadratic(double a, double b, double d) {
         double x1;
         double x2;
         x1 = (-b + Math.sqrt(d)) / (2 * a);
@@ -61,10 +74,16 @@ public class CalcEquation {
         return new double[]{x1, x2};
     }
 
-    private static double solutionQudratic(double a, double b) {
+    /*
+        Method that calculates the root of quadratic equation if discriminant = 0
+    */
+    private static double solutionQuadratic(double a, double b) {
         return -b / 2 * a;
     }
 
+    /*
+        Method that calculates the root of  equation if coefficient a = 0 -> quadratic equation converts into equation b*x+c=0
+    */
     private static double solutionSimple(double b, double c) {
         return -c / b;
     }
